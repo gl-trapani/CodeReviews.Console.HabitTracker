cc4cb50c2229b434fcb33b540d4b6fb666d25288
luca <trapani@preishoheit.com>
2025-11-20 11:43:06 +0100

    feat: select logic

--- HabitTracker/UserInterface/SelectMenu.cs
+++ HabitTracker/UserInterface/SelectMenu.cs
@@ -1,9 +1,9 @@
+using HabitTracker.Habits;
 using HabitTracker.Repositories;
-using Microsoft.Extensions.Options;
 
 namespace HabitTracker.UserInterface;
 
-public class SelectMenu (BaseRepo exerciseRepo)
+public class SelectMenu(BaseRepo exerciseRepo)
 {
     private BaseRepo _exerciseRepo = exerciseRepo;
 
@@ -13,17 +13,29 @@ public class SelectMenu (BaseRepo exerciseRepo)
         Options();
     }
 
-    public void Display()
+    private void Display()
     {
         Console.Clear();
         Console.WriteLine("1 - Display all records");
         Console.WriteLine("2 - Display by habits");
-        
     }
 
-    public void Options()
+    private void Options()
     {
-        
+        var input = Input.ValidInt();
+
+        switch (input)
+        {
+            case 1:
+                var exercises = _exerciseRepo.Select();
+                foreach (var exercise in exercises)
+                {
+                    exercise.Print();
+                }
+
+                Console.WriteLine("Press any key to return to main menu");
+                Console.ReadKey();
+                break;
+        }
     }
-    
 }
