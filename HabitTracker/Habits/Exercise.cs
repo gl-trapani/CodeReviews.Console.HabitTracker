8fdfc2dafbd0f6624eb40687db232f75307316c8
luca <trapani@preishoheit.com>
2025-11-20 11:41:20 +0100

    feat: print habit content

--- HabitTracker/Habits/Exercise.cs
+++ HabitTracker/Habits/Exercise.cs
@@ -2,16 +2,22 @@ namespace HabitTracker.Habits;
 
 public class Exercise : IHabit
 {
-    private int Id;
+    private int? Id { get; set; }
     public int Quantity { get; set; }
     public string? Type { get; set; }
     public string? Date { get; set; }
 
-    public Exercise(string type, int quantity, string? date)
+    public Exercise(string type, int quantity, string? date, int id = 0)
     {
+        Id = id;
         Type = type;
         Quantity = quantity;
 
         Date = date == "now" ? DateTime.Now.ToShortDateString() : date;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Id {Id}, Exercise {Type}, Quantity {Quantity}, Date {Date}");
     }
 }
