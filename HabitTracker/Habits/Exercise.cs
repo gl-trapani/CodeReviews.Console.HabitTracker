fb8739cf2183754abebe12277c4adf3611341d49
luca <trapani@preishoheit.com>
2025-11-20 10:48:01 +0100

    feat: date input

--- HabitTracker/Habits/Exercise.cs
+++ HabitTracker/Habits/Exercise.cs
@@ -3,14 +3,15 @@ namespace HabitTracker.Habits;
 public class Exercise : IHabit
 {
     private int Id;
-    private string Date;
     public int Quantity { get; set; }
-    public string Type { get; set; }
-    public Exercise(string type, int quantity)
+    public string? Type { get; set; }
+    public string? Date { get; set; }
+
+    public Exercise(string type, int quantity, string? date)
     {
         Type = type;
         Quantity = quantity;
+
+        Date = date == "now" ? DateTime.Now.ToShortDateString() : date;
     }
-
-
 }
